29b3756e7da7a964b646f2ce08f4f3c318ac5709
YanivRidel <117529227+Yanivridel@users.noreply.github.com>
2025-05-04 17:39:25 +0300

    add

---
+++ 1-Easy/0217-ContainsDuplicate.js
@@ -0,0 +1,10 @@
+// Given an integer array nums, return true if any value appears at least twice 
+// in the array, and return false if every element is distinct.
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    const s = new Set(nums); return s.size !== nums.length
+};
